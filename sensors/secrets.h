8db421a850269e8ecb36e46958c09b0df7fb4f96
Gracy <76453899+gracywhelihan@users.noreply.github.com>
2023-12-13 12:50:58 -0500

    Create secrets.h
    secrets.h file needs to be included with the Arduino sketch to connect to the wifi and the MQTT broker.

---
+++ sensors/secrets.h
@@ -0,0 +1,5 @@
+// #define SECRET_SSID "" // wifi network name
+// #define SECRET_PASS "" // wifi network passwork
+// broker login if applicable
+//#define SECRET_MQTT_USER "" //broker username
+//#define SECRET_MQTT_PASS "" //broker password
